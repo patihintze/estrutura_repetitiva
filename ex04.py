738e5800f689646764aad211090e8ada870523aa
patihintze <patihintze@outlook.com>
2024-08-25 19:35:48 -0300

    ajustando while

--- ex04.py
+++ ex04.py
@@ -1,8 +1,7 @@
-print("Digite os valores das coordenadas X e Y: ")
-x = int(input())
-y = int(input())
-
-while x != 0 and y != 0:
+while True:
+    print("Digite os valores das coordenadas X e Y: ")
+    x = int(input())
+    y = int(input())
     if x > 0 and y > 0:
         print("Q1")
     elif x < 0 and y > 0:
@@ -11,7 +10,6 @@ while x != 0 and y != 0:
         print("Q3")
     elif x > 0 and y < 0:
         print("Q4")
-    print("Digite os valores das coordenadas X e Y: ")
-    x = int(input())
-    y = int(input())
-    
+    if not x != 0 and y != 0:
+        break
+
